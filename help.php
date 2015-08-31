61e7ef63edd636e4754279004516df16073cd847
simisos <simon-brun@bluewin.ch>
2015-08-31 19:03:32 +0200

    casesensitve
    fixed casesensitve

---
+++ help.php
@@ -0,0 +1,43 @@
+
+
+ <?php
+require_once 'head.php'
+    ?>   
+
+<body>
+   <?php
+require_once 'navbar.php'
+    ?>
+    <div class="container own-content page-content mainborder hero-spacer">
+    <div class = "Titles">
+    <br>
+    <h1><strong> Welcome to our help !</strong></h1>
+    
+    <h3> On this Website, you can create Builds that you can then import into your gamefolder. Doing this will show them as ingame shop recommendations.</h3>
+   </div>
+    <br><br>
+    <img src='./images/index.png' width="500px"align="left">
+    <br><br><br><br>
+  
+    <h3 >First you'll have to choose a champion to do a build for. You can either press the edit button to create a new build or you can show all the builds created by other users by pressing the show builds button.</h3>
+<br><br><br><br><br><br><br><br><br><br><br>
+ <img src='./images/builder.png' width="500px"align="left">
+
+ <h3 >This is the edit screen. You will have to set a title for your build, next choose the map the build should be shown on. The group title will be the title for this item group, for example "starting items". By pressing the "Add a new group button" you can do different groups, for example "Items against AD" "Items against AP" etc. You can now drag items from the search list to the left into the square in the middle. The button to the left of the search bar will open a filter list. When you're finished just press the save button. Your build will be downloaded as a .zip file. </h3 >
+ <br><br><br><br> 
+ <div style="float: left;">
+     
+ 
+  <h1 class = "title2">
+
+      Importing your build
+  
+  </h1 >
+   <br> <br>
+   <h3 class = "title2" >
+       
+     You'll have to open the downloaded "build.zip" inside you'll find a directory . Now the last step is to copy the folder with the Build in it into your League of Legends directory ex:"C:\Riot Games\League of Legends\Config\Champions"
+   </h3 >
+  </div>
+ </div>
+</body>
